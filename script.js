e9049a2be1530c9dbf5e1e10b1603d494b0bc070
Nilesh Bhaskar <73060066+NileshBhaskar@users.noreply.github.com>
2023-10-22 10:05:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let btn = document.getElementById('enterBtn');
+                    
+btn.addEventListener('click',function(){
+	let p = document.getElementById('status');
+	if(p){
+		// p.style.display = 'none';
+		let h = document.createElement('h1');
+		h.textContent = p.textContent;
+		p.parentNode.replaceChild(h,p);
+	}
+})
+
