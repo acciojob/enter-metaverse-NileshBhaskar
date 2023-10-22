da9045f629cf6bbb48e998de37fbf16243ea3fad
Nilesh Bhaskar <73060066+NileshBhaskar@users.noreply.github.com>
2023-10-22 10:36:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-//your JS code here. If required.
 let btn = document.getElementById('enterBtn');
-                    
-btn.addEventListener('click',function(){
-	let p = document.getElementById('status');
-	if(p){
-		let h = document.createElement('h1');
-		h.setAttribute('id','status');
-		h.textContent = "Entered Metaverse";
-		p.parentNode.replaceChild(h,p);
-	}
-})
 
+btn.addEventListener('click', function() {
+  let p = document.getElementById('status');
+  if (p) {
+    let h = document.createElement('h1');
+    h.textContent = "Entered Metaverse";
+    p.parentNode.replaceChild(h, p);
+  }
+});
