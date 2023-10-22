d2048a7dd22704cc46777d320f640116d4e94fd4
Nilesh Bhaskar <73060066+NileshBhaskar@users.noreply.github.com>
2023-10-22 10:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ let btn = document.getElementById('enterBtn');
 
 btn.addEventListener('click', function() {
   let p = document.getElementById('status');
-  if (p) {
+  // if (p) {
     let h = document.createElement('h1');
-	// h.setAttribute('id','status');
     h.textContent = "Entered Metaverse";
-    // p.parentNode.replaceChild(h, p);
 	p.textContent = "";
 	p.appendChild(h);
-  }
+  // }
 });
